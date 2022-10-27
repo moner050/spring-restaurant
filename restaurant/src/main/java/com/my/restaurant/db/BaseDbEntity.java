b4bec5351a3f978d41748eef4be1b612b2d7f38d
moner050 <minhyung0237@gmail.com>
2022-10-28 02:42:59 +0900

    Feat: Create BaseDbEntity

---
+++ restaurant/src/main/java/com/my/restaurant/db/BaseDbEntity.java
@@ -0,0 +1,12 @@
+package com.my.restaurant.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BaseDbEntity {
+    protected Integer index;
+}
