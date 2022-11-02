833d48b412898647c9f49ab30b67573afdbc83a1
moner050 <minhyung0237@gmail.com>
2022-11-02 18:57:38 +0900

    MOVE: Move Directory

--- restaurant/src/main/java/com/my/restaurant/wishlist/respository/WishListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.my.restaurant.wishlist.respository;
-
-import com.my.restaurant.db.BaseDbRepositoryAbstract;
-import com.my.restaurant.wishlist.model.entity.WishListEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class WishListRepository extends BaseDbRepositoryAbstract<WishListEntity> {
-}
